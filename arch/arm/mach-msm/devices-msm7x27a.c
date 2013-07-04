b96e4c494522b2decc913a3b27db2130f215fae7
pinkflozd <lk@varjanta.com>
2013-07-04 12:43:21 +0200

    Revert "msm: kgsl: add GPU Overclock"
    This reverts commit 7b9ff743758740eca109626a8fff4fcdefc2f53b.

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -908,17 +908,6 @@ static struct resource kgsl_3d0_resources[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
-		#ifdef CONFIG_KGSL_GPU_BACONMAKER
-		{
-			.gpu_freq = 400000000,
-			.bus_freq = 220000000,
-		},
-		#elif CONFIG_KGSL_GPU_OVERCLOCK
-		{
-			.gpu_freq = 350000000,
-			.bus_freq = 210000000,
-		},
-		#else
 		/* LGE_CHANGE_S [peter.jung@lge.com]
 		    DoU Power consumption */
 		{
@@ -926,7 +915,6 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 			.bus_freq = 200000000,
 		},
 		/* LGE_CHANGE_E [peter.jung@lge.com] */
-		#endif
 		{
 			.gpu_freq = 245760000,
 			.bus_freq = 200000000,
@@ -965,19 +953,11 @@ struct platform_device msm_kgsl_3d0 = {
 void __init msm7x25a_kgsl_3d0_init(void)
 {
 	if (cpu_is_msm7x25a() || cpu_is_msm7x25aa() || cpu_is_msm7x25ab()) {
-		#ifdef CONFIG_KGSL_GPU_OVERCLOCK
-		kgsl_3d0_pdata.num_levels = 2;
-		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 166662500;
-		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 170000000;
-		kgsl_3d0_pdata.pwrlevel[1].gpu_freq = 120000000;
-		kgsl_3d0_pdata.pwrlevel[1].bus_freq = 0;
-		#else
 		kgsl_3d0_pdata.num_levels = 2;
 		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 133330000;
 		kgsl_3d0_pdata.pwrlevel[0].bus_freq = 160000000;
 		kgsl_3d0_pdata.pwrlevel[1].gpu_freq = 96000000;
 		kgsl_3d0_pdata.pwrlevel[1].bus_freq = 0;
-		#endif
 	}
 }
 
