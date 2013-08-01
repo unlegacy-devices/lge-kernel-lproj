07d52eec62b9e73641b5c3f470b9f2f4d77ab4d7
pinkflozd <lk@varjanta.com>
2013-08-01 11:05:21 +0200

    Revert "return to stock adreno frequencies"
    This reverts commit 834425ff7653024477f1212b435585ccc5d8e59c.

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -908,6 +908,13 @@ static struct resource kgsl_3d0_resources[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
+		/* LGE_CHANGE_S [peter.jung@lge.com]
+		    DoU Power consumption */
+		{
+			.gpu_freq = 320000000,
+			.bus_freq = 200000000,
+		},
+		/* LGE_CHANGE_E [peter.jung@lge.com] */
 		{
 			.gpu_freq = 245760000,
 			.bus_freq = 200000000,
@@ -922,7 +929,10 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 0,
-	.num_levels = 3,
+	/* LGE_CHANGE_S [peter.jung@lge.com]
+	     DoU Power consumption */
+	.num_levels = 4,
+	/* LGE_CHANGE_S [peter.jung@lge.com] */
 	.set_grp_async = set_grp_xbar_async,
 	.idle_timeout = HZ,
 	.strtstp_sleepwake = true,
