49f09492efeaf1c189c8f95985c76c25156b913f
pinkflozd <lk@varjanta.com>
2013-08-29 00:55:58 +0200

    Tweaks

--- arch/arm/mach-msm/devices-msm7x27a.c
+++ arch/arm/mach-msm/devices-msm7x27a.c
@@ -908,13 +908,14 @@ static struct resource kgsl_3d0_resources[] = {
 
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
-		/* LGE_CHANGE_S [peter.jung@lge.com]
-		    DoU Power consumption */
+		{
+			.gpu_freq = 380000000,
+                        .bus_freq = 245760000,
+		},
 		{
 			.gpu_freq = 355760000,
                         .bus_freq = 213760000,
 		},
-		/* LGE_CHANGE_E [peter.jung@lge.com] */
 		{
 			.gpu_freq = 320000000,
 			.bus_freq = 200000000,
@@ -931,7 +932,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.init_level = 0,
 	/* LGE_CHANGE_S [peter.jung@lge.com]
 	     DoU Power consumption */
-	.num_levels = 4,
+	.num_levels = 5,
 	/* LGE_CHANGE_S [peter.jung@lge.com] */
 	.set_grp_async = set_grp_xbar_async,
 	.idle_timeout = HZ,
